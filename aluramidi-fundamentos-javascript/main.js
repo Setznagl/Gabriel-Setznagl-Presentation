11e49cc1a79214a3dde8cece31646c5988995e4e
Gabriel Setznagl <91721431+Setznagl@users.noreply.github.com>
2023-04-14 01:10:21 -0300

    14/04/2023

--- aluramidi-fundamentos-javascript/main.js
+++ aluramidi-fundamentos-javascript/main.js
@@ -1,23 +1,20 @@
-/* Resolução individual 
-var teclas_buttons = ['.tecla_pom','.tecla_clap','.tecla_tim','.tecla_puff','.tecla_splash','.tecla_toim','.tecla_psh',
-'.tecla_tic','.tecla_tom'];
-var sons_buttons = ['#som_pom','#som_clap','#som_tim','#som_puff','#som_splash','#som_toim','#som_psh','#som_tic',
-'#som_tom'];
-document.querySelector('.tecla_pom').onclick = () => {buttons(0)};
-document.querySelector('.tecla_clap').onclick = () => {buttons(1)};
-document.querySelector('.tecla_tim').onclick = () => {buttons(2)};
-document.querySelector('.tecla_puff').onclick = () => {buttons(3)};
-document.querySelector('.tecla_splash').onclick = () => {buttons(4)};
-document.querySelector('.tecla_toim').onclick = () => {buttons(5)};
-document.querySelector('.tecla_psh').onclick = () => {buttons(6)};
-document.querySelector('.tecla_tic').onclick = () => {buttons(7)};
-document.querySelector('.tecla_tom').onclick = () => {buttons(8)};
-function buttons (x){document.querySelector(sons_buttons[x]).play();}
-/* Resolução da Aula */ 
-for (ListaDasTeclas = 0; ListaDasTeclas <= ListaDasTeclas.length; ListaDasTeclas+=1){
-    return z
-}
 const ListaDasTeclas = document.querySelectorAll('.tecla'); 
-const ListaDosSons = document.querySelectorAll('.som');
-function buttons(x) {ListaDosSons[x].play();}
-ListaDasTeclas[z].onclick = () => {buttons(z)};
+
+function tocar (SeletorAudio){
+    ChecklistSeletorAudio = document.querySelector(SeletorAudio);
+    if (ChecklistSeletorAudio === null){console.log('elemento não encontrado')}
+    if (ChecklistSeletorAudio != null && ChecklistSeletorAudio.localName === 'audio')
+    {ChecklistSeletorAudio.play();}
+    else{console.log('elemento não encontrado')}
+};
+for (contador = 0; contador < ListaDasTeclas.length; contador += 1)
+{   const tecla_atual = ListaDasTeclas[contador];
+    const ferramentaT = tecla_atual.classList[1];
+    const template_string_audio = `#som_${ferramentaT}`;
+    tecla_atual.onclick = function () { tocar(template_string_audio) };
+
+    tecla_atual.onkeydown = function (gatilho_tecla) {
+        if(gatilho_tecla.code === "Enter" || gatilho_tecla.code === "Space") {
+             tecla_atual.classList.add('ativa') }}       
+    tecla_atual.onkeyup = function () {tecla_atual.classList.remove('ativa')}
+}
