f9ee691182fadc6ff76d5e2a3f40fa93a5382cb5
Gabriel PCA <gabrielembraer3@gmail.com>
2023-09-17 18:38:26 -0300

    formatando diretorios

---
+++ POKEDEX/assets/js/main.js
@@ -0,0 +1,10 @@
+console.log("Loading!");
+
+// Exemplo de requisição de API via FETCH API JS
+
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10';
+
+const offset = 0;
+const limit =10;
+
+fetch(url);
